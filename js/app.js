91c53a8781a9875a51ae7ab10385d7f16880e78b
Cédric Rollo <cedric.rollo@iut-tlse3.fr>
2019-07-03 13:22:09 +0200

    :construction: suppression question

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 const form = document.querySelector('form');
 const table = document.querySelector('tbody');
+const efface = document.querySelectorAll('.del');
 console.log(table);
 
 form.addEventListener('submit', e => {
@@ -9,7 +10,20 @@ form.addEventListener('submit', e => {
   const td = document.createElement('td');
   const question = document.createTextNode(form.newQuestion.value);
   td.appendChild(question);
+  td.innerHTML += '<i class="small material-icons del">delete_outline</i>';
   tr.appendChild(td);
   table.appendChild(tr);
   form.reset();
+});
+
+table.addEventListener('click', e => {
+  console.log(e.target);
+  e.target.classList.toggle('reponse');
+})
+
+const poubelles = Array.from(efface);
+poubelles.forEach(poubelle => {
+  poubelle.addEventListener('click', (e) => {
+    poubelle.parentNode.removeChild(e.target);
+  })
 });
