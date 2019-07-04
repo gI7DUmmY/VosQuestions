444f343ecd92c5777626c12a6e2100fde278c1ea
Cédric Rollo <cedric.rollo@iut-tlse3.fr>
2019-07-04 12:01:55 +0200

    :sparkles: :bulb: :lipstick: efface question + vide tableau + blur submit

--- js/app.js
+++ js/app.js
@@ -1,29 +1,29 @@
 const form = document.querySelector('form');
 const table = document.querySelector('tbody');
-const efface = document.querySelectorAll('.del');
-console.log(table);
+const btnSub = document.querySelector('form button[type="submit"]');
+const btnClr = document.querySelector('button.clear');
 
+// génère question dans tableau
+const template = question => {
+  const html = `
+    <tr><td>${question}<i class="small material-icons del">delete_outline</i></td></tr>
+  `;
+  table.innerHTML += html;
+};
+
+// submit question
 form.addEventListener('submit', e => {
   e.preventDefault();
-  console.log(form.newQuestion.value);
-  const tr = document.createElement('tr');
-  const td = document.createElement('td');
-  const question = document.createTextNode(form.newQuestion.value);
-  td.appendChild(question);
-  td.innerHTML += '<i class="small material-icons del">delete_outline</i>';
-  tr.appendChild(td);
-  table.appendChild(tr);
+  template(form.newQuestion.value);
   form.reset();
+  btnSub.blur();
 });
 
+// toggle question répondue ou suppr question
 table.addEventListener('click', e => {
-  console.log(e.target);
-  e.target.classList.toggle('reponse');
-})
-
-const poubelles = Array.from(efface);
-poubelles.forEach(poubelle => {
-  poubelle.addEventListener('click', (e) => {
-    poubelle.parentNode.removeChild(e.target);
-  })
+  if (!e.target.classList.contains('del')) e.target.classList.toggle('reponse');
+  if (e.target.classList.contains('del')) e.target.parentElement.parentElement.remove();
 });
+
+// vider tableau des questions
+btnClr.addEventListener('click', () => table.innerHTML = '');
